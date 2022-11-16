6fd580bd3c44b15a331d85898a07eadf19e8abdc
malwareami! <46420533+malicious-dev@users.noreply.github.com>
2022-11-17 00:25:28 +0530

    header done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from './components/Header'
 
+import "./styles/App.scss";
+import "./styles/header.scss"
 
 function App() {
   return (
    <Router>
+    <Header />
     <Routes>
       <Route />
     </Routes>
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+
+
+const Header = () => {
+  return (
+    <nav>
+  <h1>TechStar.</h1>      
+  <main>
+    <Link to={"/"}>Home</Link>
+    <Link to={"/#contacts"}>Contact</Link>
+    <Link to={"/#about"}>About</Link>
+    <Link to={"/#brands"}>Brands</Link>
+    <Link to={"/"}>Services</Link>
+  </main>
+    </nav>
+  )
+}
+
+export default Header
